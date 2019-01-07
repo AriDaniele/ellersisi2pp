1fc092b917e68b4b8831cade6bf43f6bef3c6d2b
AriDaniele <arielandreas.daniele@students.salle.url.edu>
2019-01-07 19:14:49 +0100

    modificat main.c
    quan obrim un fitxer ens hem d'assegurar sempre de tancar-lo. Per això he afegit un else del if que comprova si no s'ha pogut obrir el fitxer, per tal de que hi entri si s'ha obert. Al final d'aquest else tancarem el fitxer amb la línia fclose(f)

--- main.c
+++ main.c
@@ -11,35 +11,36 @@ int main() {
 	int option;
 
 	FILE* f;
-	f = fopen(PATH, "r");
+	
+    f = fopen(PATH, "r");
 
 	if (f == NULL) {
 		printFileError(PATH);
 		return -1;
+	}else{
+		List l = create();
+	    readWholeFile(f, &l);
+
+	    printWelcomeLine();
+
+	    do {
+	        printMenu();
+
+	        do {
+	            option = askUserForOption();
+
+	            if (!isCorrect(option)) {
+	                printOptionError();
+	            }
+	        } while (!isCorrect(option));
+
+	        execute(option, &l);
+	    } while (!isExit(option));
+
+	    fclose(f);
+	    destroy(&l);
 	}
-
-	List l = create();
-
-	readWholeFile(f, &l);
-
-
-	printWelcomeLine();
-
-	do {
-		printMenu();
-
-		do {
-			option = askUserForOption();
-
-			if (!isCorrect(option)) {
-				printOptionError();
-			}
-		} while (!isCorrect(option));
-
-		execute(option, &l);
-	} while (!isExit(option));
-
-	destroy(&l);
+	
 
 	return 0;
 }
